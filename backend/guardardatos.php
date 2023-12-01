83c1b75db894eac462aedfc700e94954a5add4b0
jorgeluismosqueram <jluismosquera@miuniclaretiana.edu.co>
2023-12-01 16:50:01 -0500

    commit jorge

---
+++ backend/guardardatos.php
@@ -0,0 +1,48 @@
+<?php 
+$conexion = mysqli_connect("localhost","root","","tbm");
+if (!$conexion) {
+    echo "error conexión". mysqli_connect_error();
+    exit();
+}
+
+#VERIFICAMOS SI LAS CONTRASEÑAS COINCIDEN
+if ($_POST['contraseña'] != $_POST['contraseña2']) {
+ 
+    header("Location: registro.php?e=1");
+ } else {
+    #ENCRIPTAR LA CONTRASEÑA
+ $contraseña_encriptada = password_hash($_POST['contraseña2'], PASSWORD_DEFAULT);
+ 
+ #3 Crear comando sql 
+ $sql = "insert into usuarios(documento,nombres,apellidos,fecha,correo,telefono,contraseña,fechar) values 
+ ('".$_POST['nombre']."',
+ '".$_POST['apellido']."',
+ '".$_POST['documento']."',
+ '".$_POST['fecha']."',
+ '".$_POST['email']."',
+ '".$_POST['phone']."',
+  '".$contraseña_encriptada."',
+  '".date('Y-m-d')."')";
+ 
+ #4 Ejecutar 
+ $resultado =mysqli_query($conexion, $sql);
+ echo mysqli_error($conexion);
+ 
+ #5 Verificar ejecución
+  if ($resultado) {
+    // echo "Se guardó la información";
+     header("Location: registro.php?correcto=1");
+     #aquí debe redigir a la otra actividad
+  }else{
+     echo "Hubo un error: ". mysqli_error($conexion);
+  }
+ }
+/*
+$nombre = $_POST['nombre'];
+$apellido =$_POST['apellido'];
+$documento = $_POST['documento'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$fecha_de_nacimiento = $_POST['fecha'];
+$contraseña1 = $_POST['contraseña'];
+$contraseña2 = $_POST['contraseña2'];*/
---
+++ backend/login.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+#1 Establecer la conexión con la base 
+$conexion = mysqli_connect("localhost","root","","tbm");
+$oe= $_POST['correo'];
+
+#2Verificar conexión 
+if (!$conexion) {
+    echo "error conexión". mysqli_connect_error();
+    exit();
+}
+#3 Crear comando sql 
+$sql = "select contraseña, nombres, apellidos  from usuarios where correo='".$_POST['correo']."'";
+#4 Ejecutar 
+$resultado = mysqli_query($conexion, $sql);
+#4.1 Organizar datos
+$row = mysqli_fetch_assoc($resultado);
+
+#5 Verificar ejecución
+if ($row) {
+    #verificar la contraseña
+    $verificar_contraseña = password_verify($_POST['contrasena'], $row['contraseña']);
+    if ($verificar_contraseña==true) {
+        #aquí debe redigir a la otra actividad
+       $_SESSION['nombreusuario'] = $row['nombres']." ".$row[ 'apellidos'];
+       # $_SESSION['sesion'] == true;
+       //header("Location:problematica/");
+       echo "epa manito";
+    } else {
+       // header("Location:login.php?error=1");
+       echo "no manito";
+    }
+    
+ }else{
+    echo "Hubo un error: ". mysqli_error($conexion);
+ }
+
+?>
